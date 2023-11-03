498133a945f31266d8ed92adaa0845c68b9dd132
Vineeth Chivukula <66107997+VineethChivukula@users.noreply.github.com>
2023-11-03 07:10:16 +0530

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -23,13 +23,17 @@ if st.button("Submit"):
     st.write("Weight:", weight)
     st.write("Height:", height)
     height_m = height / 100
-    bmi = weight / (height_m ** 2)
-    st.write("Your BMI is: {:.2f}".format(bmi))
-    if bmi < 18.5:
-        st.write("You are underweight")
-    elif bmi >= 18.5 and bmi < 25:
-        st.write("You are normal weight")
-    elif bmi >= 25 and bmi < 30:
-        st.write("You are overweight")
-    else:
-        st.write("You are obese")
+    try:
+        bmi = weight / (height_m ** 2)
+    except ZeroDivisionError as e:
+        print("Please Enter Your Height")
+    else:    
+        st.write("Your BMI is: {:.2f}".format(bmi))
+        if bmi < 18.5:
+            st.write("You are underweight")
+        elif bmi >= 18.5 and bmi < 25:
+            st.write("You are normal weight")
+        elif bmi >= 25 and bmi < 30:
+            st.write("You are overweight")
+        else:
+            st.write("You are obese")
